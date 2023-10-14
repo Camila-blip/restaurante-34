c3faf5a89442a8d394df84eb2d4b2da418499406
Bruno Wake <brunowake.bw@gmail.com>
2023-10-14 14:36:00 -0300

    fix: interface method name

--- src/core/domain/repositories/IClienteRepository.ts
+++ src/core/domain/repositories/IClienteRepository.ts
@@ -4,5 +4,5 @@ import { Cliente } from "../Entities/cliente";
 
 export interface IClienteRepository {
     create(cliente: Cliente): Promise<Cliente>;
-    get(id: number): Promise<Cliente>;
+    getByCpf(cpf: string): Promise<Cliente>;
 }
