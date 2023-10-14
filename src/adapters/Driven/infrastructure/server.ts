e08b017befb996cf30cf5235ea5b6b029d844456
Bruno Wake <brunowake.bw@gmail.com>
2023-10-14 14:37:32 -0300

    feat: using cliente routes

--- src/adapters/Driven/infrastructure/server.ts
+++ src/adapters/Driven/infrastructure/server.ts
@@ -1,22 +1,31 @@
 import express from "express";
 import "dotenv/config";
 import http from "http";
-import "./database";
 import bodyParser from "body-parser";
 import cors from "cors";
-import clienteRoutes from "@/adapters/Driven/infrastructure/routes/cliente.routes";
+import ClienteRoutes from "@/adapters/Driven/infrastructure/routes/cliente.routes";
+import ClienteController from "@/adapters/controllers/cliente.controller";
+import ClienteRepository from "@/adapters/Driver/ClienteRepository";
+import { CreateClienteUseCase } from "@/core/application/useCases/cliente/ClienteUseCase";
+import {prisma} from "@/adapters/Driven/infrastructure/database";
 
 
 try{
   const app = express();
-  const cliente = new clienteRoutes(app);
 
-  cliente.buildRoutes();
-  
-  app.use(bodyParser.json({ limit: "50mb" }));
+  const clienteRepository = new ClienteRepository(prisma);
+  const createClienteUseCase = new CreateClienteUseCase(clienteRepository);
+  const clienteController = new ClienteController(createClienteUseCase);
+  const clienteRoutes = new ClienteRoutes(app, clienteController);
+
+  app.use(bodyParser.json({ limit: "50mb" })); 
   app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
   app.use(express.json());
   app.use(cors());
+
+  
+  clienteRoutes.buildRoutes();
+
   
   const server = http.createServer(app);
   
