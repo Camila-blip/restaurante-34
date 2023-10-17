4333c0cef896e873a0d33207698d4bea466b0f16
Camila Moura <cmiranda@fastprobr.com>
2023-10-16 22:49:57 -0300

    feat: model prisma

--- prisma/migrations/20231014151334_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `dataNascimento` on the `Cliente` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Cliente" DROP COLUMN "dataNascimento",
-ADD COLUMN     "data" TIMESTAMP(3);
